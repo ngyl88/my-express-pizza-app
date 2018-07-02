ab9328692c3c0315170216953fe083029946cb2b
ngyl88 <ng_yl88@hotmail.com>
2018-07-02 16:31:10 +0800

    Define port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 // const bodyParser = require("body-parser");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 // app.use(bodyParser.json());
